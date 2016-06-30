837394e3cb90d74884dab2022052f89d9a2ee0eb
Barry <lizhe_email@sina.com>
2016-07-01 05:51:17 +0800

    Project is created.

---
+++ SHOperation/SHOperation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SHOperation
+//
+//  Created by Li Zhe on 7/1/16.
+//  Copyright © 2016 SH10. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SHOperation/SHOperation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SHOperation
+//
+//  Created by Li Zhe on 7/1/16.
+//  Copyright © 2016 SH10. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
